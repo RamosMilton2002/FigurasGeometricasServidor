dfb48c008e277a1ce3a1a9ccb8a169060129e466
Milton <milton.ramos954@ist17dejulio.edu.ec>
2024-12-17 15:50:17 -0500

    Creacion para el calculo del area del tringulo y cambio de nombre de la ruta del cuadrado del area

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ app.use(express.json());
 
 
 
-app.get('/area/:lado', (req, resp) => {
+app.get('/cuadrado/:lado', (req, resp) => {
     let lado = parseInt(req.params.lado);
         let area = lado * lado;
         resp.send('El área del cuadrado es: ' + area);
+});
+
+app.get('/triangulo/:base/:altura', (req, resp) => {
+    let base = parseFloat(req.params.base);
+    let altura = parseFloat(req.params.altura); 
+
+
+        let area = (base * altura) / 2; 
+        resp.send('El área del triángulo es: ' + area);
 });
 
 app.listen(3001,()=>{
